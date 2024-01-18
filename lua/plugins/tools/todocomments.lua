7d52c639412eb5be7eb55128e91ab86607959ea0
Kelvin Philip <kelvin.philip@amd.com>
2024-01-18 10:22:03 -0500

    Todo Comments

---
+++ lua/plugins/tools/todocomments.lua
@@ -0,0 +1,20 @@
+local status_ok, todocomments = pcall(require, 'todo-comments')
+if not status_ok then
+    return
+end
+
+todocomments.setup()
+
+vim.keymap.set("n", "]t", function()
+  require("todo-comments").jump_next()
+end, { desc = "Next todo comment" })
+
+vim.keymap.set("n", "[t", function()
+  require("todo-comments").jump_prev()
+end, { desc = "Previous todo comment" })
+
+-- You can also specify a list of valid jump keywords
+
+vim.keymap.set("n", "]t", function()
+  require("todo-comments").jump_next({keywords = { "ERROR", "WARNING" }})
+end, { desc = "Next error/warning todo comment" })
