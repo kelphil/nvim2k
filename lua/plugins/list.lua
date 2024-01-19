9dbc5c58e71c76cefa473a2b419884b13873add4
Kelvin Philip <kelvin.philip@amd.com>
2024-01-19 06:41:30 -0500

    Keymaps

--- lua/plugins/list.lua
+++ lua/plugins/list.lua
@@ -380,8 +380,9 @@ return {
 	},
 	{
 		"folke/which-key.nvim",
+        lazy = true,
 		config = load_config("tools.which-key"),
-		event = "VeryLazy",
+		keys = "<leader>",
 	},
 	{
 		"iamcco/markdown-preview.nvim",
@@ -401,20 +402,12 @@ return {
 		"epwalsh/obsidian.nvim",
 		version = "*", -- recommended, use latest release instead of latest commit
 		lazy = true,
-		-- ft = "markdown",
-		event = "BufEnter",
-		-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-		-- event = {
-		--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-		--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-		--   "BufReadPre path/to/my-vault/**.md",
-		--   "BufNewFile path/to/my-vault/**.md",
-		-- },
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"hrsh7th/nvim-cmp",
 		},
 		config = load_config("tools.obsidian"),
+        keys = "<leader>n"
 	},
 	{
 		"HakonHarnes/img-clip.nvim",
