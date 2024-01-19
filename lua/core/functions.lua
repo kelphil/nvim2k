d8e21a7c68eb02a7a22610cca2b199894b670746
Kelvin Philip <kelvin.philip@amd.com>
2024-01-19 06:20:35 -0500

    Find and Replace Command

--- lua/core/functions.lua
+++ lua/core/functions.lua
@@ -54,3 +54,17 @@ vim.api.nvim_set_keymap(
 	"<Cmd>lua require('telescope.builtin').commands()<CR>",
 	{ noremap = false }
 )
+
+-- Find and Replace
+vim.api.nvim_create_user_command("FindAndReplace", function(opts)
+  vim.api.nvim_command(string.format("cdo %%s/%s/%s", opts.fargs[1], opts.fargs[2]))
+  vim.api.nvim_command("cfdo update")
+end, { nargs = "*" })
+
+
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>ir",
+  ":FindAndReplace ",
+  { noremap = true }
+)
